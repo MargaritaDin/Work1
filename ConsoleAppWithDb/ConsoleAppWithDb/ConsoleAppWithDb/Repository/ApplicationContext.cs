24b863b022d3347f01f7e34113c7cfa30cdf9b28
Sergii Chervonyi <chervoniys@gmail.com>
2018-03-06 23:20:13 +0200

    Add test EF app

---
+++ ConsoleAppWithDb/ConsoleAppWithDb/ConsoleAppWithDb/Repository/ApplicationContext.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.Entity;
+using ConsoleAppWithDb.DAO;
+
+namespace ConsoleAppWithDb.Repository
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("ApplicationContext")
+        {
+        }
+
+        public DbSet<Postashalnik> Postashalniks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Postashalnik>()
+                .ToTable("Pracivnuku");
+
+            modelBuilder.Entity<Postashalnik>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Postashalnik>()
+                .Property(u => u.Id)
+                .HasColumnName("ID_Pracik");
+
+            modelBuilder.Entity<Postashalnik>()
+                .Property(u => u.FirstName)
+                .HasColumnName("Name");
+
+            modelBuilder.Entity<Postashalnik>()
+                .Property(u => u.LastName)
+                .HasColumnName("Last_Name");
+        }
+
+    }
+}
